b24bcfb82ce42a45cae022f63c8d09c5dd09173a
Kamran-Muradov <kamranshmu@code.edu.az>
2024-07-20 15:04:57 +0400

    Add project files.

---
+++ Food-Delivery-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Food_Delivery_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
